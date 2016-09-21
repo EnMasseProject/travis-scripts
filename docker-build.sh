ca3a1ab9c6eeef4d061ad79206e9cbf7652c53d9
Ulf Lilleengen <lulf@redhat.com>
2016-09-21 15:05:50 +0200

    Add missing repo to push

--- docker-build.sh
+++ docker-build.sh
@@ -17,5 +17,5 @@ then
     docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS || exit 1
     docker tag $REPO:$COMMIT $REPO:$TAG || exit 1
     docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER || exit 1
-    docker push || exit 1
+    docker push $REPO || exit 1
 fi
