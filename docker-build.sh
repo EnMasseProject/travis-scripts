b211c684a75d45d4ccded2e30ce7f5e36f304e70
Ulf Lilleengen <lulf@redhat.com>
2016-10-10 11:45:24 +0200

    Remove deprecated email parameter

--- docker-build.sh
+++ docker-build.sh
@@ -14,7 +14,7 @@ docker build -t $REPO:$COMMIT $DIR || exit 1
 
 if [ "$TRAVIS_BRANCH" == "master" ] || [ -n "$TRAVIS_TAG" ]
 then
-    docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS || exit 1
+    docker login -u $DOCKER_USER -p $DOCKER_PASS || exit 1
     docker tag $REPO:$COMMIT $REPO:$TAG || exit 1
     docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER || exit 1
     docker push $REPO || exit 1
