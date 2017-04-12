99ca083f23a99f22f8121ff64908b2cdbbdad941
Ulf Lilleengen <lulf@redhat.com>
2017-04-12 12:03:24 +0200

    Use separate repo for snapshots and releases

--- upload-artifact.sh
+++ upload-artifact.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 # Assumes that BINTRAY_API_USER and BINTRAY_API_KEY to be set
-REPOSITORY=$1
+PACKAGE=$1
 ARTIFACT=$2
 TAG="latest"
+REPOSITORY="snapshots"
 
 if [ -n "$TRAVIS_TAG" ]
 then
     TAG="$TRAVIS_TAG"
+    REPOSITORY="releases"
 fi
 
 FILENAME=`basename $ARTIFACT`
@@ -17,6 +19,7 @@ if [ "$TRAVIS_BRANCH" == "master" ] || [ -n "$TRAVIS_TAG" ]
 then
     if [ "$TRAVIS_PULL_REQUEST" == "false" ]
     then
-        curl -T $ARTIFACT -u${BINTRAY_API_USER}:${BINTRAY_API_KEY} "https://api.bintray.com/content/enmasse/artifacts/${REPOSITORY}/${TAG}/${TAG}/${FILENAME}?publish=1&override=1"
+
+        curl -T $ARTIFACT -u${BINTRAY_API_USER}:${BINTRAY_API_KEY} "https://api.bintray.com/content/enmasse/${REPOSITORY}/${PACKAGE}/${TAG}/${TAG}/${FILENAME}?publish=1&override=1"
     fi
 fi
