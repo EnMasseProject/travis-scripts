eaf34a64bdb2546040c79e3b3a5a7d546830070e
Ulf Lilleengen <lulf@redhat.com>
2017-05-29 13:37:49 +0200

    Support replacing multiple components

--- run-tests.sh
+++ run-tests.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-COMPONENT=$1
-COMMIT=$2
+COMMIT=$1
+COMPONENTS=${@:2}
 
 pushd systemtests
 sleep 20
 export PATH=$PATH:/tmp/openshift
 cat /etc/hosts
 curl -L https://raw.githubusercontent.com/EnMasseProject/enmasse/master/install/openshift/enmasse.yaml -o enmasse.yaml
-sed -i -e "s/${COMPONENT}:latest/${COMPONENT}:${COMMIT}/g" enmasse.yaml
+for COMPONENT in $COMPONENTS
+do
+    sed -i -e "s/${COMPONENT}:latest/${COMPONENT}:${COMMIT}/g" enmasse.yaml
+done
 
 ./scripts/run_test_travis.sh enmasse.yaml
 ./scripts/print_logs.sh
