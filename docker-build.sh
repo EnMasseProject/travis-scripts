e38327fe369a9d4ad7a26b81635b77da040ab4b9
Ulf Lilleengen <lulf@redhat.com>
2017-05-31 13:53:44 +0200

    Print image push info

--- docker-build.sh
+++ docker-build.sh
@@ -13,4 +13,5 @@ docker build --build-arg version=$VERSION -t $REPO:$COMMIT $DIR || exit 1
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS || exit 1
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER || exit 1
+echo "Pushing $REPO:$COMMIT"
 docker push $REPO || exit 1
